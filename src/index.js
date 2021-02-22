a3fc37bc1cffcb7afad19b7a2c12e850d3313fdb
Osvaldo Arzate <osvarz.new@gmail.com>
2021-02-22 10:28:59 -0600

    avocados print created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import './style/style.css';
 
-console.log('hi world with webpack');
+const app = document.querySelector('#app');
+
+const createCardAvocado = data => {
+  const cards = [];
+  console.log(data.data)
+  data.data.forEach(avocado => {
+    const container = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = `https://platzi-avo.vercel.app${avocado.image}`;
+    const title = document.createElement('h2');
+    title.textContent = avocado.name;
+    const price = document.createElement('p')
+    price.textContent = avocado.price;
+    container.append(img, title, price);
+    cards.push(container);
+  });
+  app.append(...cards)
+};
+
+const getData = async URL => {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    createCardAvocado(data)
+  } catch (err) {
+    console.error(err.message)
+  }
+};
+
+getData('https://platzi-avo.vercel.app/api/avo')
 
 if (module.hot) {
   module.hot.accept()

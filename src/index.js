e45421028e34724378c7efd3f2471747549827d3
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2021-02-24 09:54:26 -0600

    added styles

--- src/index.js
+++ src/index.js
@@ -1,36 +1,48 @@
-import './style/style.css';
+import "./style/style.css";
 
-const app = document.querySelector('#app');
+const app = document.querySelector("#app");
 
-const createCardAvocado = data => {
+const formatPrice = (price) => {
+  const newPrice = window.Intl.NumberFormat("en-EN", {
+    style: "currency",
+    currency: "USD",
+  }).format(price);
+
+  return newPrice;
+};
+
+const createCardAvocado = (data) => {
   const cards = [];
-  console.log(data.data)
-  data.data.forEach(avocado => {
-    const container = document.createElement('div');
-    const img = document.createElement('img');
+  console.log(data.data);
+  data.data.forEach((avocado) => {
+    const container = document.createElement("div");
+    container.className =
+      "m-3 pb-3 text-center max-w-1/4 inline-block bg-gray-200 rounded-xl";
+
+    const img = document.createElement("img");
     img.src = `https://platzi-avo.vercel.app${avocado.image}`;
-    const title = document.createElement('h2');
+    img.className = "m-auto pb-3";
+
+    const title = document.createElement("h2");
     title.textContent = avocado.name;
-    const price = document.createElement('p')
-    price.textContent = avocado.price;
+
+    const price = document.createElement("p");
+    price.textContent = formatPrice(avocado.price);
+
     container.append(img, title, price);
     cards.push(container);
   });
-  app.append(...cards)
+  app.append(...cards);
 };
 
-const getData = async URL => {
+const getData = async (URL) => {
   try {
     const response = await fetch(URL);
     const data = await response.json();
-    createCardAvocado(data)
+    createCardAvocado(data);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
 };
 
-getData('https://platzi-avo.vercel.app/api/avo')
-
-if (module.hot) {
-  module.hot.accept()
-};
+getData("https://platzi-avo.vercel.app/api/avo");
